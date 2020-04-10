4c60ff2dd703f659ce03ed8e49667d5fd67706db
Oliver Lyman <admin@oliverlyman.net>
2020-04-09 22:02:31 -0400

    Add basic layout

---
+++ .eleventy.js
@@ -0,0 +1,7 @@
+const inclusiveLangPlugin = require("@11ty/eleventy-plugin-inclusive-language");
+const navigationPlugin = require("@11ty/eleventy-navigation");
+
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPlugin(inclusiveLangPlugin);
+  eleventyConfig.addPlugin(navigationPlugin);
+};
--- index.md
+++ index.md
@@ -1,3 +1,10 @@
+---
+layout: layouts/base.njk
+eleventyNavigation:
+  key: Home
+  order: 1
+---
+
 # Oliver Lyman's Site
 
 This is a collections of my thoughts on a variety of topics.
