95eabdb6589147b3d49329219d60d3332e0ae922
oslyman <admin@oliverlyman.net>
2020-04-13 22:34:29 +0000

    Add footnotes to markdown and post draft

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,14 @@
 const { DateTime } = require("luxon");
 const inclusiveLangPlugin = require("@11ty/eleventy-plugin-inclusive-language");
+const markdownItPlugin = require("markdown-it");
+const markdownItFootnotePlugin = require("markdown-it-footnote");
 const navigationPlugin = require("@11ty/eleventy-navigation");
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(inclusiveLangPlugin);
   eleventyConfig.addPlugin(navigationPlugin);
+
+  eleventyConfig.setLibrary("md", markdownItPlugin().use(markdownItFootnotePlugin))
 
   eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.setDataDeepMerge(true);
